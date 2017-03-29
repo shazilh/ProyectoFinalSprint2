16243b1fca439a6d58274feaa2b9b734a27a2a61
Shazil Tovar <shazil@gmail.com>
2017-03-29 13:18:43 -0600

    segundovers trello

--- trello.js
+++ trello.js
@@ -1,18 +1,35 @@
 function agregarNombrePendiente(){
     var nombrePendiente=document.getElementById("nombrePendiente").value;
+    var visualizar=document.getElementById("visualizarPendiente");
+    //var contenedor=document.getElementById("contenedorPendientes");
+    var preview =document.getElementById("preview");
+    var nuevoNodo=preview.cloneNode(true);
+
     var pendientes = document.createElement("article");
     pendientes.innerHTML=nombrePendiente==''?'(nada)':nombrePendiente;
     document.getElementById("visualizarPendiente").appendChild(pendientes);
+    pendientes.style.fontWeight = "900";
+
     document.getElementById("nombrePendiente").value = "";
+    document.getElementById("preview").innerText="";
+
+    visualizar.insertBefore("nuevoNodo,visualizar.firstChild");
 
 }
+
+function mostrar(){
+  var texto=document.getElementById("nombrePendiente").value;
+  document.getElementById("preview").innerHTML=texto;
+}
+
 /*
-var texto=document.getElementById("liname").value;
-var li=document.createElement("LI");
-li.innerHTML=texto==''?'(nada)':texto;//Este texto va a ser un nuevo elemento
-document.getElementById("lista").appendChild(li);//Ese nuevo elemento metelo como hijo a mi lista
-document.getElementById("liname").value = "";//pa
-/*
+//para clonar
+var nuevoPendiente =pendientes.cloneNode(true);
+    nuevoPendiente.id=Date.now();
+
+    pendientes.insertBefore("nuevoPendiente,pendientes.firstChild");
+
+
 function crearListas(){
       var nombreListaAAgregar=document.getElementById('nombreLista');
       var nuevaLista=document.createElement("ol");
